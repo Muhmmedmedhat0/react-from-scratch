dafd4a19b9775fd9003b7235596086e62304e73b
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-02-28 00:49:03 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/app.css';
+function App() {
+  return (
+    <div className="app">
+      <h1>React from scratch&#128153;.</h1>
+    </div>
+  );
+}
+
+export default App;
